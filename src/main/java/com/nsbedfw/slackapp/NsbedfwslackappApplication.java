f9253f486fa6add66e82aaab44576ec490a487a9
Goodness Daramola <gdaramol@thoughtworks.com>
2019-07-18 07:29:28 -0400

    init commit

---
+++ src/main/java/com/nsbedfw/slackapp/NsbedfwslackappApplication.java
@@ -0,0 +1,13 @@
+package com.nsbedfw.slackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsbedfwslackappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsbedfwslackappApplication.class, args);
+	}
+
+}
