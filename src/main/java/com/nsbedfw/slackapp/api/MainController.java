542d7246f9547d397c9b34e380db3c418a07fb95
Goodness Daramola <gdaramol@thoughtworks.com>
2019-09-17 21:53:35 -0400

    updating controller

--- src/main/java/com/nsbedfw/slackapp/api/MainController.java
+++ src/main/java/com/nsbedfw/slackapp/api/MainController.java
@@ -2,24 +2,31 @@ package com.nsbedfw.slackapp.api;
 
 import com.nsbedfw.slackapp.services.slack.SlackResponse;
 import com.nsbedfw.slackapp.services.slack.SlackService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Service
 @Path("/api")
 public class MainController{
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @Autowired
     SlackService slackService;
 
     @POST
-    @Produces("application/json")
-    public SlackResponse getEvents(@RequestParam SlackRequestPayload request) {
-        SlackResponse response = slackService.createResponse(request.getText());
+    @Consumes(APPLICATION_FORM_URLENCODED)
+    @Produces(APPLICATION_JSON)
+    public SlackResponse getEvents(@FormParam("text") String text) {
+        log.info("slackSlashCommand: {}", text);
+        SlackResponse response = slackService.createResponse(text);
         return response;
     }
 }
